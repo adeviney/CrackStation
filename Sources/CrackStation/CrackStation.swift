b38e93787298d85c65594dd8fb0783d312cbedf8
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-10-22 18:20:11 -0700

    added test for loading data

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -6,10 +6,14 @@ public class CrackStation {
     
     enum CrackStationError: Error {
         case failedToLoadHashFromDisk(String)
-        case hashNotFound(String)
+        // case hashNotFound(String) - not currently implemented
     }
 
-    public init?() throws {
+    public init() throws {
+        self.mappingToPlaintext = try CrackStation.loadDictionaryFromFile()
+    }
+    
+    static func loadDictionaryFromFile() throws -> [String: String] {
         guard let filePath = Bundle.module.path(forResource: "HashtoPlaintextData", ofType: "json") else {
             throw CrackStationError.failedToLoadHashFromDisk("Could not find data resource in Bundle")
         }
@@ -19,23 +23,22 @@ public class CrackStation {
         
         
         if let dictionaryFromJson: [String: String] = try? JSONDecoder().decode(Dictionary<String, String>.self, from: jsonData) {
-            self.mappingToPlaintext = dictionaryFromJson
+            return dictionaryFromJson
         } else {
             throw CrackStationError.failedToLoadHashFromDisk("Incorrect format for the data file. Make sure it is String-to-String mapping with valid JSON.")
         }
-        
     }
+    
+    
     
     /// Either returns the cracked plain-text password
     /// or, if unable to crack, then returns nil.
-    ///
-    /// Throw an error if <insert explanation here>.
-    public func crack(_ password: String) throws -> String? {
+    public func crack(_ password: String) -> String? {
         if let crackedPassword = self.mappingToPlaintext[password] {
             return crackedPassword
         }
         else {
-            throw CrackStationError.hashNotFound("\(password) could not be cracked")
+            return nil
         }
     }
 }
--- Tests/CrackstationTests/CrackstationTests.swift
+++ Tests/CrackstationTests/CrackstationTests.swift
@@ -3,16 +3,28 @@ import XCTest
 
 final class CrackStationTests: XCTestCase {
     
+    func testLoadDictionary() throws {
+        do {
+            let plaintextTable = try CrackStation.loadDictionaryFromFile()
+            XCTAssertNotNil(plaintextTable)
+            
+            let crackedpassword = plaintextTable["95cb0bfd2977c761298d9624e4b4d4c72a39974a"]
+            XCTAssert(crackedpassword == "y")
+        } catch {
+            XCTFail()
+        }
+        
+    }
     func testCanCrack_oneChar_1() throws {
         let crackstation = try CrackStation()
-        let crackedPassword = try crackstation.crack("86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
+        let crackedPassword = crackstation.crack("86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
         XCTAssert(crackedPassword == "a")
         
     }
     
     func testCanCrack_oneChar_2() throws {
         let crackstation = try CrackStation()
-        let crackedPassword = try crackstation.crack("902ba3cda1883801594b6e1b452790cc53948fda")
+        let crackedPassword = crackstation.crack("902ba3cda1883801594b6e1b452790cc53948fda")
         XCTAssert(crackedPassword == "7")
     }
 }
