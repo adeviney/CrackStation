f30227be828698bf4e4a6a9e626ba417aa81e592
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-10-22 18:10:23 -0700

    added a static function to loadDictionary that is called when CrackStation is initialized

--- Sources/Crackstation/Crackstation.swift
+++ Sources/Crackstation/Crackstation.swift
@@ -9,7 +9,11 @@ public class CrackStation {
         case hashNotFound(String)
     }
 
-    public init?() throws {
+    public init() throws {
+        self.mappingToPlaintext = try CrackStation.loadDictionaryFromFile()
+    }
+    
+    static func loadDictionaryFromFile() throws -> [String: String] {
         guard let filePath = Bundle.module.path(forResource: "HashtoPlaintextData", ofType: "json") else {
             throw CrackStationError.failedToLoadHashFromDisk("Could not find data resource in Bundle")
         }
@@ -19,12 +23,13 @@ public class CrackStation {
         
         
         if let dictionaryFromJson: [String: String] = try? JSONDecoder().decode(Dictionary<String, String>.self, from: jsonData) {
-            self.mappingToPlaintext = dictionaryFromJson
+            return dictionaryFromJson
         } else {
             throw CrackStationError.failedToLoadHashFromDisk("Incorrect format for the data file. Make sure it is String-to-String mapping with valid JSON.")
         }
-        
     }
+    
+    
     
     /// Either returns the cracked plain-text password
     /// or, if unable to crack, then returns nil.
--- Tests/CrackstationTests/CrackstationTests.swift
+++ Tests/CrackstationTests/CrackstationTests.swift
@@ -5,14 +5,14 @@ final class CrackStationTests: XCTestCase {
     
     func testCanCrack_oneChar_1() throws {
         let crackstation = try CrackStation()
-        let crackedPassword = try crackstation!.crack("86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
+        let crackedPassword = try crackstation.crack("86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
         XCTAssert(crackedPassword == "a")
         
     }
     
     func testCanCrack_oneChar_2() throws {
         let crackstation = try CrackStation()
-        let crackedPassword = try crackstation!.crack("902ba3cda1883801594b6e1b452790cc53948fda")
+        let crackedPassword = try crackstation.crack("902ba3cda1883801594b6e1b452790cc53948fda")
         XCTAssert(crackedPassword == "7")
     }
 }
